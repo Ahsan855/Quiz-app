570e1496ddd6e5558ad01731726c409025dccf58
Ahsan855e <a.r.ahsanhabib08@gmail.com>
2022-07-05 13:16:32 +0600

    done my quiz app

---
+++ src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>rhi is login</h1>
+    </div>
+  );
+};
+
+export default Login;
--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -3,10 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const Quiz = () => {
   return (
-    <div className="p-40 bg-black">
+    <div className="py-44 px-10 my-20 mx-44 bg-blue-400  rounded-full">
+      <h1 className="text-center mb-10 text-4xl text-white font-bold">
+        Keep going by clicking here
+      </h1>
       <NavLink
+        id="clickAndEnter"
         to="/quizApp"
-        className="bg-white p-2 text-2xl font-bold text-purple-500 rounded-md"
+        className="bg-white p-4 text-3xl font-bold text-purple-500 rounded-full"
       >
         Start Quiz
       </NavLink>
